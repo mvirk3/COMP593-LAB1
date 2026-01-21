ee22fe5eb132c7dcb20854199ed94126297554e0
mvirk4490 <mvirk4490@gmail.com>
2026-01-21 18:54:32 -0500

    Modified both files in the repo clone
    deleted a print statement from the script1.py and added one to script2.py.

--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 print("This is the first line for the second file.")
 print("This is the second line for the second file of code.")
+print("we are doing the part 2.")
 # Print line from zen of python
