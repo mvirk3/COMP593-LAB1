ea7e70a466a45dc98e39b016a4a8f52505418803
mvirk4490 <mvirk4490@gmail.com>
2026-01-21 18:38:20 -0500

    code added
    Code added

---
+++ script1.py
@@ -0,0 +1,2 @@
+print("This is the first line of the code.")
+print("This is the second line for the code.")
---
+++ script2.py
@@ -0,0 +1,2 @@
+print("This is the first line for the second file.")
+print("This is the second line for the second file of code.")
